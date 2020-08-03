235a530496b1eb3e76b2d7f4ebf2ce6177339fa2
Antariksh <antariksh.cloud@gmail.com>
2020-08-03 11:02:40 -0700

    MPU6050 library structure

---
+++ POC/RaspberryPi/MPU6050.cpp
@@ -0,0 +1,60 @@
+/*
+Author: Antariksh Narain
+Organization: Space Engineering Research Center (SERC), ISI, USC
+*/
+
+#include "MPU6050.h"
+#include <vector>
+#include <wiringPiI2C.h>
+#include <wiringPi.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+
+using namespace std;
+
+#pragma region PUBLIC MEMBERS
+
+MPU6050::MPU6050(int device_address)
+{
+    this->id = wiringPiI2CSetup(Device_Address);
+    wiringPiI2CWriteReg8(this->id, SMPLRT_DIV, 0x07); /* Write to sample rate register */
+    wiringPiI2CWriteReg8(this->id, PWR_MGMT_1, 0x01); /* Write to power management register */
+    wiringPiI2CWriteReg8(this->id, CONFIG, 0);        /* Write to Configuration register */
+    wiringPiI2CWriteReg8(this->id, GYRO_CONFIG, 24);  /* Write to Gyro Configuration register */
+    wiringPiI2CWriteReg8(this->id, INT_ENABLE, 0x01); /*Write to interrupt enable register */   
+}
+
+vector<float> MPU6050::RawValues()
+{
+    return vector<float>{this->Ax, this->Ay, this->Az, this->Gx, this->Gy, this->Gz, this->TempC}; 
+}
+
+vector<double> MPU6050::PrincipalAxisValues()
+{
+    this->pitch = Ax / (Ax^2 + Az^2);
+    this->roll = Ay / (Ay^2 + Az^2);
+}
+
+#pragma endregion
+
+#pragma region PRIVATE MEMBERS
+short MPU6050::registerValue(int addr)
+{
+    short high_byte, low_byte, value;
+    high_byte = wiringPiI2CReadReg8(this->id, addr);
+    low_byte = wiringPiI2CReadReg8(this->id, addr + 1);
+    value = (high_byte << 8) | low_byte;
+    return value;
+}
+void MPU6050::Calibrate(int duration)
+{
+    // TODO: Read sensor values for the given duration (seconds)
+    time_t s_time = time();
+    do
+    {
+        
+    }while(time() - s_time < duration);
+    // TODO: Calulate the mean, variance, max and min values
+}
+#pragma endregion
---
+++ POC/RaspberryPi/MPU6050.h
@@ -0,0 +1,53 @@
+/*
+Author: Antariksh Narain
+Organization: Space Engineering Research Center (SERC), ISI, USC
+*/
+#include<vector>
+
+using namespace std;
+
+#pragma region REGISTER VALUES
+#define SMPLRT_DIV      0x19
+#define CONFIG          0x1A
+#define GYRO_CONFIG     0x1B
+#define ACCEL_CONFIG    0x1C
+#define FIFO_EN         0x23
+#define INT_ENABLE      0x38
+
+#define ACCEL_XOUT_H    0x3B
+#define ACCEL_YOUT_H    0x3C
+#define ACCEL_ZOUT_H    0x3D
+#define TEMP_OUT_H      0x41
+#define GYRO_XOUT_H     0x43
+#define GYRO_YOUT_H     0x44
+#define GYRO_ZOUT_H     0x45
+
+#define PWR_MGMT_1      0x6B
+#pragma endregion
+
+class MPU6050
+{
+    private:
+    float Ax, Ay, Az;
+    float Gx, Gy, Gz;
+    float TempC;
+    int id;
+
+    private:
+    // Function to read raw register value
+    short registerValue(int addr);
+
+    protected:
+    double roll, pitch;
+
+    public:
+    // Function to Initialize with the selected device address
+    MPU6050(int device_address);
+
+    // Function to return RAW values from the sensor
+    vector<float> RawValues();
+    
+    // Function to calculate Pitch and Roll
+    vector<double> PrincipalAxisValues();
+
+};
